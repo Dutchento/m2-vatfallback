b4700f3a018fbda14313075c419ac1048c1c23e1
Jeroen Boersma <jeroen@elgentos.nl>
2019-08-13 14:13:31 +0200

    Added Configuration and interface
    + configuration interface with all paths to config.xml
    + configuration implementation

---
+++ Service/Configuration.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Dutchento\Vatfallback\Service;
+
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\StoreManagerInterface;
+
+
+class Configuration implements ConfigurationInterface
+{
+
+    /** @var StoreManagerInterface */
+    protected $storeManager;
+
+    public function __construct(
+        StoreManagerInterface $storeManager
+    ) {
+        $this->storeManager = $storeManager;
+    }
+
+    public function isViesValidation(StoreInterface $store = null): bool
+    {
+        return (bool)$this->storeManager
+            ->getStore($store)
+            ->getConfig(self::XMLPATH_CUSTOMER_VATFALLBACK_VIES_VALIDATION);
+    }
+
+    public function getViesTimeout(StoreInterface $store = null): int
+    {
+        return +$this->storeManager
+            ->getStore($store)
+            ->getConfig(self::XMLPATH_CUSTOMER_VATFALLBACK_VIES_TIMEOUT);
+    }
+
+    public function isVatlayerValidation(StoreInterface $store = null): bool
+    {
+        return (bool)$this->storeManager
+            ->getStore($store)
+            ->getConfig(self::XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_VALIDATION);
+    }
+
+    public function getVatlayerApikey(StoreInterface $store = null): string
+    {
+        return $this->storeManager
+            ->getStore($store)
+            ->getConfig(self::XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_APIKEY);
+    }
+
+    public function getVatlayerTimeout(StoreInterface $store = null): int
+    {
+        return +$this->storeManager
+            ->getStore($store)
+            ->getConfig(self::XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_TIMEOUT);
+    }
+
+}
