7c6a08c52c97645eec3d1845cd31a3e9068f8140
Timon de Groot <timon@marissen.net>
2019-10-23 14:32:11 +0200

    Fix configuration not corresponding with system.xml

--- Service/ConfigurationInterface.php
+++ Service/ConfigurationInterface.php
@@ -8,14 +8,14 @@ use Magento\Store\Api\Data\StoreInterface;
 interface ConfigurationInterface
 {
 
-    const XMLPATH_CUSTOMER_VATFALLBACK_VIES_VALIDATION = 'vatfallback/vatfallback/vies_validation';
-    const XMLPATH_CUSTOMER_VATFALLBACK_VIES_TIMEOUT = 'vatfallback/vatfallback/vies_timeout';
+    const XMLPATH_CUSTOMER_VATFALLBACK_VIES_VALIDATION = 'customer/vatfallback/vies_validation';
+    const XMLPATH_CUSTOMER_VATFALLBACK_VIES_TIMEOUT = 'customer/vatfallback/vies_timeout';
 
-    const XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_VALIDATION = 'vatfallback/vatfallback/vatlayer_validation';
-    const XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_APIKEY = 'vatfallback/vatfallback/vatlayer_apikey';
-    const XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_TIMEOUT = 'vatfallback/vatfallback/vatlayer_timeout';
+    const XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_VALIDATION = 'customer/vatfallback/vatlayer_validation';
+    const XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_APIKEY = 'customer/vatfallback/vatlayer_apikey';
+    const XMLPATH_CUSTOMER_VATFALLBACK_VATLAYER_TIMEOUT = 'customer/vatfallback/vatlayer_timeout';
 
-    const XMLPATH_CUSTOMER_VATFALLBACK_REGEXP_VALIDATION = 'vatfallback/vatfallback/regexp_validation';
+    const XMLPATH_CUSTOMER_VATFALLBACK_REGEXP_VALIDATION = 'customer/vatfallback/regexp_validation';
 
     public function isViesValidation(StoreInterface $store = null): bool;
     public function getViesTimeout(StoreInterface $store = null): int;
