bf28d1c67540793fdceb6c8271b8ca39dd16cdb0
Timon de Groot <timon@marissen.net>
2019-10-28 14:52:04 +0100

    Change API companylookup data structure

--- Api/CompanyLookup.php
+++ Api/CompanyLookup.php
@@ -9,6 +9,8 @@
 
 namespace Dutchento\Vatfallback\Api;
 
+use Dutchento\Vatfallback\Api\Data\CompanyLookupResultInterface;
+use Dutchento\Vatfallback\Api\Data\CompanyLookupResultInterfaceFactory;
 use Dutchento\Vatfallback\Service\CleanNumberString;
 use Dutchento\Vatfallback\Service\ConfigurationInterface;
 use Dutchento\Vatfallback\Service\Vatlayer\Client as VatlayerClient;
@@ -32,6 +34,10 @@ class CompanyLookup implements CompanyLookupInterface
      * @var ConfigurationInterface
      */
     private $configuration;
+    /**
+     * @var CompanyLookupResultInterfaceFactory
+     */
+    private $resultFactory;
 
     /**
      * CompanyLookup constructor.
@@ -39,23 +45,26 @@ class CompanyLookup implements CompanyLookupInterface
      * @param LoggerInterface $logger
      * @param CleanNumberString $cleanNumberString
      * @param ConfigurationInterface $configuration
+     * @param CompanyLookupResultInterfaceFactory $resultFactory
      */
     public function __construct(
         VatlayerClient $vatlayerClient,
         LoggerInterface $logger,
         CleanNumberString $cleanNumberString,
-        ConfigurationInterface $configuration
+        ConfigurationInterface $configuration,
+        CompanyLookupResultInterfaceFactory $resultFactory
     ) {
         $this->vatlayerClient = $vatlayerClient;
         $this->logger = $logger;
         $this->cleanNumberString = $cleanNumberString;
         $this->configuration = $configuration;
+        $this->resultFactory = $resultFactory;
     }
 
     /**
      * @inheritdoc
      */
-    public function byVatnumber(string $vatNumber): array
+    public function byVatnumber(string $vatNumber): CompanyLookupResultInterface
     {
         $country = substr($vatNumber, 0, 2);
         $cleanVatnumber = $this->cleanNumberString->returnStrippedString($vatNumber);
@@ -70,21 +79,25 @@ class CompanyLookup implements CompanyLookupInterface
             );
             $data = json_decode($response->getBody(), true);
 
-            return [
-                'status' => $data['valid'] ?? false,
-                'country' => $data['country_code'] ?? 'Unknown',
-                'company_name' => $data['company_name'] ?? 'Unknown',
-                'company_address' => $data['company_address'] ?? 'Unknown',
-                'message' => $data['error']['message'] ?? '',
-            ];
+            return $this->resultFactory->create([
+                'data' => [
+                    'status' => $data['valid'] ?? false,
+                    'country' => $data['country_code'] ?? 'Unknown',
+                    'company_name' => $data['company_name'] ?? 'Unknown',
+                    'company_address' => $data['company_address'] ?? 'Unknown',
+                    'message' => $data['error']['message'] ?? '',
+                ],
+            ]);
         } catch (\Exception $exception) {
             $this->logger->error($exception->getMessage());
             $message = $exception->getMessage();
         }
 
-        return [
-            'status' => false,
-            'message' => $message
-        ];
+        return $this->resultFactory->create([
+            'data' => [
+                'status' => false,
+                'message' => $message,
+            ],
+        ]);
     }
 }
